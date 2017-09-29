47e947cdf2b6e4048eccd10b4e27fe4a1aaf54aa
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-09-29 15:15:34 -0400

    Added a new integration test for signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: {
+        user: {
+          name:  'Example User',
+          username: 'username_example',
+          email: 'user@example.com',
+          password:              'password',
+          password_confirmation: 'password'
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_equal flash[:success], 'Welcome to the CTFDashB'
+  end
 end

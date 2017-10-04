979cfc63df199c4e71d49a5a7a3368dba3f2e490
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-10-04 12:00:46 -0400

    added validations for the capture the flag model

--- app/models/capture_the_flag.rb
+++ app/models/capture_the_flag.rb
@@ -1,2 +1,5 @@
 class CaptureTheFlag < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :info, presence: true
+  validates :max_teammates, presence: true
 end

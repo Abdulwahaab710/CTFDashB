dc74ae3a738e0ea2cc8c24f907d42249a312bbb3
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-10-18 16:41:07 -0400

    added new methods in the user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,41 @@
+# User controller
 class UsersController < ApplicationController
+  before_action :user_logged_in?, only: [:edit]
+  include SessionsHelper
+
   def new
+    if logged_in?
+      redirect_back_or current_user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to the CTFDashB'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :username,
+      :email,
+      :password,
+      :password_confirmation
+    )
   end
 end

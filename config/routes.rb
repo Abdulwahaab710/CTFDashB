937c8dcffa9cc14f37e58dbc6d9b3370dae58311
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-10-18 16:35:41 -0400

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get '/teams/new', to: 'teams#new', as: :new_team
+  post '/teams/new', to: 'teams#create'
+
+  get '/teams/join', to: 'teams#join', as: :join_team
+  post '/teams/join', to: 'teams#join_team'
+
+  get '/sessions/new'
+
+  get '/signup', to: 'users#new', as: :signup
+  post '/signup', to: 'users#create'
+
+  get '/users/:id', to: 'users#show', as: :user
+  delete '/users/:id', to: 'users#deactivate', as: :deactivate_user
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

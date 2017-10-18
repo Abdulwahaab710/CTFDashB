d60bd1d42c9980a614d136cce41cd83effcdd7b9
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-10-18 16:48:45 -0400

    rake db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018203648) do
+ActiveRecord::Schema.define(version: 20171018204828) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "ip_address"
+    t.string   "browser"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"

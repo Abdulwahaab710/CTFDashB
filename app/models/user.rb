430b6540490bf4f39b314d9fe85ed54270469642
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-10-18 16:39:01 -0400

    added validations for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+# User models
 class User < ApplicationRecord
+  before_save { email.downcase! }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+  validates :username,
+            presence: true,
+            length: { maximum: 255 },
+            uniqueness: { case_sensitive: false }
+  validates :password,
+            presence: true,
+            length: { minimum: 6 }
+  has_secure_password
 end

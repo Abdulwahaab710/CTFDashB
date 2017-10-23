f5095c2ff1013f47f906dca60929ca9bf786bed4
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-10-22 22:32:04 -0400

    db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018210406) do
+ActiveRecord::Schema.define(version: 20171019030736) do
 
   create_table "challenges", force: :cascade do |t|
     t.float    "points"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20171018210406) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "ctf_settings", force: :cascade do |t|
+    t.string   "key"
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "sessions", force: :cascade do |t|

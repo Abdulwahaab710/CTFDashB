9ce04e355b5b9c12c31b5b47908c3b95ad8cd2de
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-11-06 18:39:47 -0500

    fixing Code style

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,12 @@ class SessionsController < ApplicationController
   include SessionsHelper
 
   def new
-    if logged_in?
-      redirect_back_or current_user
-    else
-      render :new
-    end
+    redirect_back_or current_user if logged_in?
+    render :new
   end
 
   def create
-    # TODO ALLOW USERS TO LOGIN VIA USERNAME OR EMAIL
+    # TODO: ALLOW USERS TO LOGIN VIA USERNAME OR EMAIL
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user

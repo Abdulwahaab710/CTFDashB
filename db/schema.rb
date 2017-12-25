ee904953a2d532c1d3117667210617e47fa33ed3
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-12-25 17:46:51 -0500

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023125838) do
+ActiveRecord::Schema.define(version: 20171225224354) do
+
+  create_table "challenge_categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "challenges", force: :cascade do |t|
     t.float    "points"

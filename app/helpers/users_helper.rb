e299e7ee3a9cecb81131a893717b433224c9893c
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2017-12-25 19:10:13 -0500

    added user picture in the nav

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,17 @@
 # User helper
 module UsersHelper
-  def gravatar_for(user, size: 80)
+  def gravatar_for(user, size: 80, margin: nil)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    margin = margin.nil? == false ? "margin: #{margin}" : ''
+    puts gravatar_url
+    image_tag(
+      gravatar_url,
+      alt: user.name,
+      class: 'gravatar',
+      width: size,
+      height: size,
+      style: "border-radius: 4px; #{margin}"
+    )
   end
 end

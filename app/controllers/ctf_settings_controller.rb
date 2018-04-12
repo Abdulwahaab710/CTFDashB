21fcc18e61b148e3dfbe4ef655b574274698b8c5
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-04-12 18:38:44 -0400

    added a success flash for settings

--- app/controllers/ctf_settings_controller.rb
+++ app/controllers/ctf_settings_controller.rb
@@ -1,15 +1,11 @@
 class CtfSettingsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
   def edit
     settings_params.each do |param|
       setting = CtfSetting.find_or_create_by!(key: param[0])
       setting.update_attributes(value: param[1])
     end
+    flash[:success] = 'Settings has been updated'
+    redirect_to action: 'show'
   end
 
   def show

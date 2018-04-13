aa3416af1ea1c5f425d8d7d85d12383268a129fc
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-04-13 18:07:27 -0400

    fixing logo and removing organizers

--- db/migrate/20171226041644_create_organizers.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrganizers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :organizers do |t|
-      t.references :User, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end

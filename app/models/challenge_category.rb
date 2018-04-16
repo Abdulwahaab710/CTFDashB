fad31dc2eb7b9af48555178cfb4a0b49255d9fcf
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-04-15 21:42:46 -0400

    cleaning up some files

--- app/models/challenge_category.rb
+++
@@ -1,3 +0,0 @@
-class ChallengeCategory < ApplicationRecord
-  has_many :challenges
-end
--- db/migrate/20171225224712_add_challenge_categories_to_challenges.rb
+++
@@ -1,5 +0,0 @@
-class AddChallengeCategoriesToChallenges < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :challenges, :challenge_category, foreign_key: true
-  end
-end

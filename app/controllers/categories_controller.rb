588ddc97526705aa68be7f8d7862801f15a9ff78
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-04-15 22:50:27 -0400

    category controller and views

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,25 @@
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(challenge_params)
+    render :new unless @category.save
+  end
+
+  def edit
+    @category = Category.update_attr
+  end
+
+  def destroy
+    @category = Category.find_by(id: params[:id])
+    @category.destroy
+  end
+
+  private
+
+  def challenge_params
+    params.require(:category).permit(:name)
+  end
+end

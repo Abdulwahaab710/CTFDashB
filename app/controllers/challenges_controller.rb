bb613d7493619bc65dc35c9fccfab939c07412a3
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-04-15 21:26:37 -0400

    added new methods to the challenges controller

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,28 +1,38 @@
 class ChallengesController < ApplicationController
   before_action :user_logged_in?, except: [:index]
 
-  def new
-    @challenge = Challenge.new
-  end
-
-  def create
-    @challenge = Challenge.create!(challenge_params)
-  end
-
   def index
     @challenges = Challenge.all
   end
 
+  def new
+    @challenge = Challenge.new
+    @category = Category.order(:name)
+  end
+
+  def create
+    @challenge = Challenge.new(challenge_params)
+    redirect_to @challenge if @challenge.save
+    render :new unless performed?
+  end
+
   def show
+    @challenge = Challenge.find_by(id: params[:id])
   end
 
   def destroy
+    @challenge = Challenge.find_by(id: params[:id])
+    @challenge.destroy
   end
 
   def activate
+    @challenge = Challenge.find_by(id: params[:id])
+    @challenge.activate
   end
 
   def deactivate
+    @challenge = Challenge.find_by(id: params[:id]).deactivate
+    @challenge.activate
   end
 
   private
@@ -33,7 +43,9 @@ class ChallengesController < ApplicationController
       :description,
       :link,
       :points,
-      :max_tries
+      :max_tries,
+      :active,
+      :category_id
     )
   end
 end

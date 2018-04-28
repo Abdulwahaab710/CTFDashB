c4b52bcf8b9747cf1270b0fbced45e18e421cdcb
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-04-28 19:13:38 -0400

    Added model validation

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,13 @@
 class Challenge < ApplicationRecord
   belongs_to :category
 
-  validates :title, :description, :points, :max_tries, :category, presence: true
+  validates :title, :description, :points, :max_tries, :category, :flag, presence: true
   validates :points, :max_tries, numericality: { greater_than_or_equal_to: 1 }
+  validates :flag, uniqueness: true
+
+  def flag=(flag)
+    write_attribute(:flag, BCrypt::Password.create(flag))
+  end
 
   def activate
     self.active = true

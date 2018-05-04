bc739a62ddc768a81ea377f75d599edba480ec3e
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-03 22:04:39 -0400

    Fixing routes for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,18 @@
 class CategoriesController < ApplicationController
-  before_action :user_logged_in?, except: %i[index]
+  before_action :user_logged_in?, except: %i[show]
   before_action :user_has_permission?, except: %i[index show]
+  before_action :fetch_categories
 
   include Users
 
   def index
-    @categories = Category.order(:name)
+    @categories = Category.all
+    @challenges = Challenge.where(active: true)
+    return render 'challenges/index' unless current_user&.organizer?
   end
 
   def show
-    @category = Category.find_by(id: params[:id])
+    @category = Category.find_by!(id: params[:id])
   end
 
   def new
@@ -23,23 +26,27 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find_by(id: params[:id])
+    @category = Category.find_by!(id: params[:id])
   end
 
   def update
-    @category = Category.find_by(id: params[:id])
+    @category = Category.find_by!(id: params[:id])
     render :edit unless @category.update_attributes(category_params)
     redirect_to @category unless performed?
   end
 
   def destroy
-    @category = Category.find_by(id: params[:id])
+    @category = Category.find_by!(id: params[:id])
     redirect_to :show unless @category.destroy
     flash[:success] = "You have successfully delete #{@category.name}"
     redirect_to categories_path unless performed?
   end
 
   private
+
+  def fetch_categories
+    @categories = Category.all
+  end
 
   def category_params
     params.require(:category).permit(:name)

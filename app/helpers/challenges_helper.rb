3c9d892d2e7f72f490d6660321d03b361e6d24f0
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-05 10:28:48 -0400

    Fixing links path for challenge status buttons

--- app/helpers/challenges_helper.rb
+++ app/helpers/challenges_helper.rb
@@ -4,13 +4,23 @@ module ChallengesHelper
     deactivate_challenge_button(challenge)
   end
 
+  private
+
   def deactivate_challenge_button(challenge)
-    challenge_status_button('Deactivate challenge', 'btn-danger', deactivate_challenge_path(challenge))
+    challenge_status_button(
+      'Deactivate challenge',
+      'btn-danger',
+      deactivate_category_challenge_path(challenge.category, challenge)
+    )
   end
 
 
   def activate_challenge_button(challenge)
-    challenge_status_button('Activate challenge', 'btn-success', activate_challenge_path(challenge))
+    challenge_status_button(
+      'Activate challenge',
+      'btn-success',
+      activate_category_challenge_path(challenge.category, challenge.category)
+    )
   end
 
   def challenge_status_button(btn_name, btn_color, path)

3059ffa523bea2c8b203738c3d53fb6524d802e3
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-05 10:25:36 -0400

    Using ruby safe navigation operator

--- app/controllers/concerns/users.rb
+++ app/controllers/concerns/users.rb
@@ -2,6 +2,6 @@ module Users
   extend ActiveSupport::Concern
 
   def user_has_permission?
-    render file: "#{Rails.root}/public/404.html", status: 403, layout: false unless current_user.organizer?
+    render file: "#{Rails.root}/public/404.html", status: 403, layout: false unless current_user&.organizer?
   end
 end

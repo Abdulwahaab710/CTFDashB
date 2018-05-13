7f3932b5471253984dc113dc7f514235fe9c5bd3
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-13 13:11:41 -0400

    added user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create!(
+      name: 'James bond',
+      email: 'James.bond@mi6.gov',
+      username: '007',
+      password: 'my amazing password'
+    )
+  end
+
+  context 'when creating a user' do
+    it 'is valid with valid attributes' do
+      expect(@user).to eq(User.last)
+    end
+
+    it 'enforces uniqueness of users username' do
+      expect(
+        User.new(
+          name: 'James bond',
+          email: 'James.bond2@mi6.gov',
+          username: '007',
+          password: 'my amazing password'
+        ).valid?
+      ).to eq false
+    end
+
+    it 'enforces uniqueness of users email' do
+      expect(
+        User.new(
+          name: 'James bond',
+          email: 'James.bond@mi6.gov',
+          username: '008',
+          password: 'my amazing password'
+        ).valid?
+      ).to eq false
+    end
+  end
 end

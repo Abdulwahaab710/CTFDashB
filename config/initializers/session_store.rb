3f8ba9f1dcde5ce72404c148c724afe92f50dddb
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-14 14:50:10 -0400

    updating to rails 5.0.7

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_CTFDashB_session'
+Rails.application.config.session_store :cookie_store, key: '_ctf_dash_b_session'

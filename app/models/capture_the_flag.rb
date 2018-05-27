748e4b4d5706ae2c9b323f9f99bc10cdd16ea332
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-27 15:48:11 -0400

    removed unused model capture the flags

--- app/models/capture_the_flag.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CaptureTheFlag < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 255 }
-  validates :info, presence: true
-  validates :max_teammates, presence: true
-end

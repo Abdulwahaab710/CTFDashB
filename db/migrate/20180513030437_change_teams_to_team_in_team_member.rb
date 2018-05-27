73a09350bf8af8ab6d1dabcea1093fc4fadbe8fc
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-05-27 16:02:08 -0400

    cleaning

--- db/migrate/20180513030437_change_teams_to_team_in_team_member.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeTeamsToTeamInTeamMember < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :ctf_points, :team, foreign_key: true
-    add_reference :ctf_points, :user, foreign_key: true
-  end
-end
--- db/migrate/20180514193138_remove_teams_from_team_members.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveTeamsFromTeamMembers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :ctf_points, :teams, :boolean
-    remove_column :ctf_points, :users, :boolean
-  end
-end

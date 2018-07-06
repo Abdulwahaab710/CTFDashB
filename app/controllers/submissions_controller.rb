9ef6707d8b4deb328c47b8b320c0dbe2dfc5fc6e
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-07-06 16:41:26 -0400

    Flag submission (controller)

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,26 +1,33 @@
 # frozen_string_literal: true
 
 class SubmissionsController < ApplicationController
-
   def create
     @challenge = Category.find_by(id: params[:category_id]).challenges.find_by(id: params[:id])
-    if @challenge
-      flag = BCrypt::Password.new(@challenge&.flag)
-      if submitted_flag == flag
-        puts 'hello'
-      else
-        respond_to do |f|
-          f.js { render json: 'Invalid flag', status: :unprocessable_entity }
-        end
-      end
-    else
-      head 404
-    end
+    return head 404 if @challenge.nil?
+    verify_flag
   end
 
   private
 
+  def verify_flag
+    flag = BCrypt::Password.new(@challenge&.flag)
+    if flag == submitted_flag
+      flash[:success] = 'Woohoo, you have successfully submitted your flag'
+      render_alert
+    else
+      respond_to do |f|
+        f.js { render js: 'unsuccessful_submission', status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def render_alert
+    respond_to do |f|
+      f.js { render 'successful_submission', status: :ok }
+    end
+  end
+
   def submitted_flag
-    params.require(:submission).permit(:flag)
+    params.require(:submission).permit(:flag)[:flag]
   end
 end

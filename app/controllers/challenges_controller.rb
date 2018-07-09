2897a2fd5dfb9999a7d716caa20fc9a83608eb80
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-07-09 00:39:44 -0400

    Raise 404 if record not found

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -57,7 +57,7 @@ class ChallengesController < ApplicationController
   private
 
   def challenge
-    Category.find_by!(id: params[:category_id]).challenges.find_by(id: params[:id])
+    Category.find_by!(id: params[:category_id]).challenges.find_by!(id: params[:id])
   end
 
   def fetch_categories

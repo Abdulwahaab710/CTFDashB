4c54b811752fb6a8e3bca335e13e6c597b6fc569
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-07-09 17:16:28 -0400

    Using ruby safe navigation operator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_10_000220) do
+ActiveRecord::Schema.define(version: 2018_07_07_120510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

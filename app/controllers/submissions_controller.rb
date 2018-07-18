b45408f36d06fe941ff9892f21d361a69d7a0f31
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-07-18 19:36:20 -0400

    Verifying flag format for submission

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -4,23 +4,36 @@ class SubmissionsController < ApplicationController
   def create
     @challenge = Category.find_by(id: params[:category_id]).challenges.find_by(id: params[:id])
     return head 404 if @challenge.nil?
+    verify_flag_format
     add_submission
     verify_flag
   end
 
   private
 
+  def verify_flag_format
+    return invalid_flag unless submitted_flag =~ CtfSetting.find_by(key: 'flag_regex')
+  end
+
   def verify_flag
     flag = BCrypt::Password.new(@challenge&.flag)
     if flag == submitted_flag
       flash[:success] = 'Woohoo, you have successfully submitted your flag'
-      @submission.update_attrubites(valid_submission: true)
+      @submission.update(valid_submission: true)
       render_alert
     else
-      @submission.update_attrubites(valid_submission: true, flag: submitted_flag)
+      flash[:danger] = 'Flag is incorrect'
+      @submission.update(valid_submission: false, flag: submitted_flag)
       respond_to do |f|
-        f.js { render js: 'unsuccessful_submission', status: :unprocessable_entity }
+        f.js { render 'unsuccessful_submission', status: :unprocessable_entity }
       end
+    end
+  end
+
+  def invalid_flag
+    flash[:danger] = 'Invalid flag format'
+    respond_to do |f|
+      f.js { render 'unsuccessful_submission', status: :unprocessable_entity }
     end
   end
 
@@ -36,11 +49,11 @@ class SubmissionsController < ApplicationController
 
   def build_submission_signature
     salt = Rails.application.secrets.submission_salt
-    Digest::SHA256.hexdigest("#{@challenge.id}#{current_user.team.id}#{current_user.id}#{submitted_flag}#{salt}")
+    Digest::SHA256.hexdigest("#{@challenge.id}#{current_user&.team&.id}#{current_user.id}#{submitted_flag}#{salt}")
   end
 
   def add_submission
-    @submission = Submission.new(
+    @submission = Submission.create!(
       team: current_user.team,
       user: current_user,
       category: @challenge.category,
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 class Submission < ApplicationRecord
   belongs_to :team
   belongs_to :user
+  belongs_to :challenge
+  belongs_to :category
 end

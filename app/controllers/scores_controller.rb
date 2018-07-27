b9a8f0c29b135076cdbe400e36589e08cb44912e
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-07-27 16:58:13 -0400

    Scores controller tests

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ScoresController < ApplicationController
+  skip_before_action :user_logged_in?, only: %i[index]
+
   def index
     @teams = Team.order(:score)
   end

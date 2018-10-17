fa2804901ba4ae44027ad774f60487135b4d68dd
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2018-10-17 14:16:17 -0400

    countdown page

---
+++ app/assets/javascripts/countdown.js
@@ -0,0 +1,36 @@
+// Set the date we're counting down to
+$(document).ready(
+  () => {
+    var countDownDate = new Date($('#countdown').data('countdown').toString()).getTime();
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Output the result in an element with id="countdown"
+      var timeLeft = '';
+      if(days > 0) { timeLeft += `${days} d `; }
+      if(hours > 0) { timeLeft += `${hours} h `; }
+      if(minutes > 0) { timeLeft += `${minutes} m `; }
+      if(seconds > 0) { timeLeft += `${seconds} s `; }
+      document.getElementById("countdown").innerHTML = timeLeft;
+
+      // If the count down is over, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        location.reload
+      }
+    }, 1000);
+  }
+)

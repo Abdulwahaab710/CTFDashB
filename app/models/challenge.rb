a86fe45dd77a3ba12e05a4a8d161b874bbbfa6eb
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2019-11-03 20:33:26 -0500

    added description to category and syntax highlighting

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -23,7 +23,16 @@ class Challenge < ApplicationRecord
     private
 
     def markdown
-      @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::Safe)
+      @markdown ||= Redcarpet::Markdown.new(MarkdownRenderer, markdown_extensions)
+    end
+
+    def markdown_extensions
+      {
+        no_intra_emphasis: true,
+        autolink: true,
+        lax_spacing: true,
+        fenced_code_blocks: true
+      }
     end
   end
 

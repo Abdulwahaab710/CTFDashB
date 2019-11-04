449e35545a19b17506851c333c645de9c52c11c6
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2019-11-03 20:32:51 -0500

    added description to category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_031213) do
+ActiveRecord::Schema.define(version: 2019_11_03_180510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_031213) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "challenges", id: :serial, force: :cascade do |t|
@@ -132,7 +133,6 @@ ActiveRecord::Schema.define(version: 2019_07_29_031213) do
     t.index ["team_id"], name: "index_users_on_team_id"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "challenges", "categories"
   add_foreign_key "challenges", "users"
   add_foreign_key "hints", "challenges"

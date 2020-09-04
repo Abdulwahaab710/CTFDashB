4ffb2ac05de08369144064e2a363fce388083bf3
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2020-09-03 21:25:04 -0400

    Create Team Type

---
+++ app/graphql/types/team_type.rb
@@ -0,0 +1,10 @@
+module Types
+  class TeamType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String, null: false
+    field :invitation_token, String, null: false
+    field :score, Integer, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end

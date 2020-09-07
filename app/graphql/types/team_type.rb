0621a53855b31308043fcad564cb2877450284ba
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2020-09-06 22:48:35 -0400

    Add users to teams

--- app/graphql/types/team_type.rb
+++ app/graphql/types/team_type.rb
@@ -4,6 +4,7 @@ module Types
     field :name, String, null: false
     field :invitation_token, String, null: false
     field :score, Integer, null: true
+    field :users, UserType.connection_type, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end

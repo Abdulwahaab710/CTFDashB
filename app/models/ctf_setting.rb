ff30d9bda49b44e317f8b1b2cf42826ca6380e05
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2020-10-17 15:12:29 -0400

    Fix hash_challenge_flag

--- app/models/ctf_setting.rb
+++ app/models/ctf_setting.rb
@@ -6,7 +6,7 @@ class CtfSetting < ApplicationRecord
   class << self
     def hash_challenge_flag?
       hash_flag = self.find_or_create_by!(key: 'hash_flag')
-      hash_flag.update(value: 'true') if hash_flag&.value&.nil?
+      hash_flag.update(value: 'true') if hash_flag.value.nil?
 
       hash_flag.value.downcase == "true"
     end

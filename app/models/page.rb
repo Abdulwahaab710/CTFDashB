f343b1b2f9c9b1dcd6ea68d3b3252f3ef3356307
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2020-10-24 22:40:27 -0400

    Add content_type and include_layout to pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,23 @@
 class Page < ApplicationRecord
   validates :path, uniqueness: true
   validates :path, :html_content, presence: true
+  validates :content_type, presence: true, inclusion: { in: %w{html markdown} }
+
+  class String < SimpleDelegator
+    include Markdownable
+
+    def to_md
+      markdown.render(to_s) if self
+    end
+  end
+
+  def html_content
+    String.new(self[:html_content])
+  end
+
+  def include_layout?
+    include_layout
+  end
 
   def to_param
     path.to_s

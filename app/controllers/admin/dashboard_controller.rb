f6f951b0e231ef01c0939c855a9a0fee859cb18f
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2020-10-29 22:08:19 -0400

    Show all challenge in the dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,11 @@
 module Admin
   class DashboardController < AdminController
     def index
-      @user_challenges = current_user&.challenges&.includes(:category)&.group_by { |c| c.category }
+      if params[:show] == 'all'
+        @user_challenges = Challenge.includes(:category)&.group_by { |c| c.category }
+      else
+        @user_challenges = current_user&.challenges&.includes(:category)&.group_by { |c| c.category }
+      end
       @total_teams = Team.count
     end
   end

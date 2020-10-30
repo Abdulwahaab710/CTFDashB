d855680395f8cf936fe4497a4b57f2503c84e2ad
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2020-10-29 21:20:17 -0400

    Add render_404 method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
 
   include Sessions
   include CtfSettings
+
+  private
+
+  def render_404
+    render file: Rails.root.join('public', '404.html'), status: :not_found
+  end
 end
